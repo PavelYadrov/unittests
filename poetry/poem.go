fa6913883f36c43a3c484d29157a1288e7a1aa39
PavelYadrov <iadrovpaul@gmail.com>
2022-02-13 19:54:36 +0300

    loading

--- poetry/poem.go
+++ poetry/poem.go
@@ -1,11 +1,44 @@
 package poetry
 
+import (
+	"bufio"
+	"os"
+)
+
 type Line string
 type Stanza []Line
 type Poem []Stanza
 
 func NewPoem() Poem {
 	return Poem{}
+}
+
+func LoadPoem(name string) (Poem, error) {
+	file, err := os.Open(name)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	p := Poem{}
+
+	s := Stanza{}
+
+	scan := bufio.NewScanner(file)
+
+	for scan.Scan() {
+		l := scan.Text()
+		if l == "" {
+			p = append(p, s)
+			s = Stanza{}
+			continue
+		}
+		s = append(s, Line(l))
+	}
+	p = append(p, s)
+
+	return p, err
 }
 
 //функция не изменяем пришедший объект, так что просто без *, работаем с его копией

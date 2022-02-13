32633c54b07619c9d747efc5472c0d3ae6842b8f
PavelYadrov <iadrovpaul@gmail.com>
2022-02-13 19:41:07 +0300

    commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/PavelYadrov/unittests
+module unittests
 
 go 1.17
+
+require github.com/PavelYadrov/unittests v0.0.0-20220213123646-a71552e8f5be
--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/PavelYadrov/unittests/poetry"
 )
 
 func main() {
 	fmt.Printf("dd")
+
+	p := poetry.Poem{{"I am working on a go project,",
+		" which has a dependency on original-project.",
+		"I now want to change the behavior in this project by modifying original-project.",
+		"So I cloned github.com/y/original-project to github.com/x/my-version and replaced all",
+		"occurrence of github.com/y/original-project with github.com/x/my-version (including in mod.go)."}}
+
+	v, c := p.Stats()
+	fmt.Printf("Vovels %d and Consonants %d", v, c)
+
 }
